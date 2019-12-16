e887f573a7488b1c53752066462ad78e1af635bf
Riley Moore <rilemoore@gmail.com>
2019-12-16 03:45:00 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,17 +11,23 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 
-	def create
-	  @post = Post.new(params["post"])
-	  @post.save
-	  redirect_to post_path(@post)
-	end
-
-	def update
-	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
-	  redirect_to post_path(@post)
-	end
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to post_path(@post)
+  end
+ 
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+ 
+  private
+ 
+  def post_params(*args)
+    params.require(:post).permit(*args)
+  end
 
 	def edit
 	  @post = Post.find(params[:id])
